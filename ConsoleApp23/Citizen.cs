87662c23e248181de3306bfd856bbfb2d91f7df0
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-05-12 19:52:30 +0300

    Add files via upload

---
+++ ConsoleApp23/Citizen.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    public class Citizen
+    {
+        string _name;
+        Citizen[] Children;
+        private readonly int _fatherId;
+        readonly int id;
+        static int _numberOfCurrentCitizens;
+        const int MAXIMUM_NUMBER_OF_CITIZENS = 2000;
+
+        public int FatherId
+        {
+            get
+            {
+                return _fatherId;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public Citizen(string name, int fatherId)
+        {
+            _name = name;
+            _fatherId = fatherId;
+        }
+
+        static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine($"Number of citizens{_numberOfCurrentCitizens}");
+        }
+        static bool ReachedOutOfMaximumSize()
+        {
+            if (_numberOfCurrentCitizens >= MAXIMUM_NUMBER_OF_CITIZENS)
+                return true;
+            else
+                return false;
+        }
+
+        void PrintId()
+        {
+            Console.WriteLine($"id:{this.id}");
+        }
+        void PrintGapBetweenMyIDAndFather()
+        {
+            Console.WriteLine($"the gap number between the father id to my id is { _fatherId}-{ this.id}");
+        }
+
+        public void SetChildren(Citizen[] children)
+        {
+            this.Children = children;
+        }
+        public Citizen[] GetChildren()
+        {
+            return this.Children;
+        }
+
+        public override string ToString()
+        {
+            return $"name {_name},father id{_fatherId},id {id}";
+        }
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Citizen Father = new Citizen("Eyal", 100);
+            Citizen Son1 = new Citizen("Dan", 100);
+            Citizen Son2 = new Citizen("Ron", 100);
+            Citizen Son3 = new Citizen("Guy", 100);
+
+            Citizen[] EyalChildren = new Citizen[] { Son1, Son2, Son3 };
+
+            Father.SetChildren(EyalChildren);
+
+        }
+
+        static bool CheckValidity(Citizen c)
+        {
+            foreach (Citizen child in c.GetChildren())
+            {
+                if (child.FatherId != c.Id)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
